21c1caefa5ec5f51fd9fa1adcd72ab0d08b09033
Gopinadendla11 <gopinadendla1@gmail.com>
2022-12-05 17:48:14 -0500

    forward slash issue

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ st.write(line_chart)
 st.subheader('Choropleth Map of Zillow Housing Prices in the US')
 st.write("This visulaization gives insights into housing prices in different states of United States over the past 10 years. You can select any year or month from the below dropdown to view the average housing prices.")
 
-df_all_homes_states = pd.read_csv("Datasets\States\sale_prices_state.csv")
+df_all_homes_states = pd.read_csv("Datasets/States/sale_prices_state.csv")
 df_all_homes_states['StateName'] = df_all_homes_states['RegionName'].map(us_state_to_abbrev)
 state_codes = [1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56]
 state_abv = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
